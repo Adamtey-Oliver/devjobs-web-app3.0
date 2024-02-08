6dbc73e9e38fc087bf44701165d667b40dbfd257
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2024-02-08 13:27:17 +0000

    Create devjobs-header.component.ts

---
+++ src/app/devjobs-header/devjobs-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-devjobs-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './devjobs-header.component.html',
+  styleUrl: './devjobs-header.component.css'
+})
+export class DevjobsHeaderComponent {
+
+}
