24301ae96add47466ea2bb8fc1c4c551e1128c72
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2024-02-12 13:57:20 +0000

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FilterBarComponent } from './filter-bar/filter-bar.component'; 
 import { DevjobsCardComponent } from './devjobs-card/devjobs-card.component'; 
+import { HeaderContentComponent } from './header-content/header-content.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, DevjobsCardComponent, FilterBarComponent],
+  imports: [CommonModule, RouterOutlet, DevjobsCardComponent,
+    FilterBarComponent, HeaderContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
