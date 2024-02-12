d23e6b6c22fb28eeba83ca0655f28f823fb3207c
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2024-02-12 13:57:14 +0000

    Update header-content.component.ts

--- src/app/header-content/header-content.component.ts
+++ src/app/header-content/header-content.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header-content',
+  selector: 'header-content',
   standalone: true,
   imports: [],
   templateUrl: './header-content.component.html',
