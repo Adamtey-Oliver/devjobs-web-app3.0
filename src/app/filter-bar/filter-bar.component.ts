a4e8c2f7a0d7325aed29c825b26b99b2e2576f38
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2024-02-19 11:21:34 +0000

    Update filter-bar.component.ts

--- src/app/filter-bar/filter-bar.component.ts
+++ src/app/filter-bar/filter-bar.component.ts
@@ -1,16 +1,86 @@
-import { Component, HostListener } from '@angular/core';
+import { ChangeDetectorRef, Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgZone } from '@angular/core';
 
 @Component({
   selector: 'filter-bar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './filter-bar.component.html',
   styleUrl: './filter-bar.component.css'
 })
 export class FilterBarComponent {
   isMobile: boolean = false;
-searchQuery: any;
+
+
+  //Filter
+  // isFilterActive: boolean = false;
+  // isSearchActive: any;
+  // constructor(private cdr: ChangeDetectorRef) {
+  //   this.checkScreenWidth();
+  // }
+
+  // checkScreenWidth() {
+  //   this.isMobile = window.innerWidth <= 768; 
+  // }
+
+  // toggleFilter() {
+  //   this.isFilterActive = !this.isFilterActive;
+  //   this.cdr.detectChanges();
+  // }
+
+  // Search Btn
+
+  // toggleSearch(): void {
+  //   if (this.isMobile) {
+      
+  //     this.isSearchActive = !this.isSearchActive;
+  //     console.log('Mobile Search Toggled');
+  //   } else {
+     
+  //     console.log('Search Button Clicked');
+  //   }
+  // }
+
+  // Filter
+isFilterActive: boolean = false;
+isSearchActive: any;
+constructor(private cdr: ChangeDetectorRef, private ngZone: NgZone) {
+  this.checkScreenWidth();
+
+  
+  window.addEventListener('resize', () => {
+    this.ngZone.run(() => {
+      this.checkScreenWidth();
+      this.cdr.detectChanges(); 
+    });
+  });
+}
+
+
+checkScreenWidth() {
+  this.isMobile = window.innerWidth <= 768; 
+}
+
+toggleFilter() {
+  this.isFilterActive = !this.isFilterActive;
+  this.cdr.detectChanges();
+}
+
+// Search Btn
+
+toggleSearch(): void {
+  if (this.isMobile) {
+    this.isSearchActive = !this.isSearchActive;
+    this.cdr.detectChanges();
+  } else {
+    console.log('Search Button Clicked');
+  }
+}
+
+  
+  searchQuery: any;
 
   @HostListener('window:resize', ['$event'])
   onResize(event: any): void {
