c867039db794b7b0f5c52c5f48d5af07e8362895
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2024-03-14 09:11:42 +0000

    update

--- src/app/job.service.ts
+++ src/app/job.service.ts
@@ -7,8 +7,8 @@ import { Jobs } from './model/jobsdata';
 })
 export class JobService {
 
-  private devjobsUrl = 'http://localhost:3000/Jobs'
-  // private devjobsUrl = 'https://64281ee346fd35eb7c4bfc31.mockapi.io/dev'
+  // private devjobsUrl = 'http://localhost:3000/Jobs'
+  private devjobsUrl = 'https://64281ee346fd35eb7c4bfc31.mockapi.io/dev'
   public isModalOpen: boolean = false 
 
 
